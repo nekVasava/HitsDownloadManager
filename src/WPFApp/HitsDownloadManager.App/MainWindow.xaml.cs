2d0093ad2538e3d5f609b82101a2719619ec8532
User <user@example.com>
2025-10-14 12:14:07 +0530

    feat: initialize project with folder structure and .gitignore
    Created full directory tree for HitsDownloadManager project

---
+++ src/WPFApp/HitsDownloadManager.App/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+namespace HitsDownloadManager.App.Views
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
